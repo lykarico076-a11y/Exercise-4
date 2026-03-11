68cc07bf937bfc1df985314571148c48d41d74e3
lvrico@paterostechnologicalcollege.edu.ph <lvrico@paterostechnologicalcollege.edu.ph>
2026-03-12 01:14:19 +0800

    Create grp2_rico_ex4.py

---
+++ grp2_rico_ex4.py
@@ -0,0 +1,40 @@
+a = 10
+b = 5
+
+print("INTEGER OPERATIONS")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+x = 10.5
+y = 2.5
+
+print("\nFLOAT OPERATIONS")
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Modulus:", x % y)
+print("Exponent:", x ** y)
+
+c1 = 3 + 2j
+c2 = 1 + 4j
+
+print("\nCOMPLEX OPERATIONS")
+print("Addition:", c1 + c2)
+print("Subtraction:", c1 - c2)
+print("Multiplication:", c1 * c2)
+print("Division:", c1 / c2)
+print("Exponent:", c1 ** 2)
+
+
+
+num1 = 25000000
+num2 = 25_000_000
+
+print("\nINTEGER LITERAL OUTPUT")
+print(num1)
+print(num2)
